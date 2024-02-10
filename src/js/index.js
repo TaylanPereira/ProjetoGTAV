d0e3f5fed91716e0dd61efe04e2c6de17745d372
TaylanPereira <155483598+TaylanPereira@users.noreply.github.com>
2024-02-10 15:38:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
